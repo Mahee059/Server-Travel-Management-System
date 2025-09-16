ccba4330d75a12f905ff5c50ffa56ed663eb14d2
Mahee059 <munakhatiwada2059@gmail.com>
2025-09-16 17:11:30 +0545

    get profile authenticate

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,10 +1,12 @@
 import express from 'express'
 import { login, register,logout, profile } from '../controllers/auth.controller'
+import { authenticate } from '../middlewares/authorization.middleware'
+import { ALLUserAndAdmins } from '../types/global.types'
 const router = express.Router()
 router.post('/register', register)
 router.post('/login', login)
 router.post('/logout', logout)
-router.get('/me', profile)
+router.get('/me', authenticate(ALLUserAndAdmins), profile)
 
 export default router; 
 
